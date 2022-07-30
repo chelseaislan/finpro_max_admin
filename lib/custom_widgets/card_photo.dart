3084620a2dde8e6e7172203bcf1d87e7ca18cd68
Rifqi R <rifqisservices@gmail.com>
2022-07-30 19:30:54 +0700

    Fix reload image

--- lib/custom_widgets/card_photo.dart
+++ lib/custom_widgets/card_photo.dart
@@ -1,6 +1,7 @@
 // @dart=2.9
 // 24/41
 import 'package:extended_image/extended_image.dart';
+import 'package:finpro_max_admin/custom_widgets/buttons/big_wide_button.dart';
 import 'package:finpro_max_admin/custom_widgets/text_styles.dart';
 import 'package:finpro_max_admin/models/colors.dart';
 import 'package:flutter/material.dart';
@@ -27,10 +28,11 @@ class CardPhotoWidget extends StatelessWidget {
             return null;
             break;
           case LoadState.failed:
-            return GestureDetector(
-              child:
-                  Center(child: DescText(text: "Reload Image", color: white)),
-              onTap: () => state.reLoadImage(),
+            return BigWideButton(
+              labelText: "Reload Image",
+              onPressedTo: () => state.reLoadImage(),
+              textColor: white,
+              btnColor: primary1,
             );
             break;
         }
